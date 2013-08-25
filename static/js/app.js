7e70ec8ba6253c5d2f294eff37686358342a1acd
Chuck Ha <ha.chuck@gmail.com>
2013-08-25 13:47:36 -0400

    send structured data

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,9 @@ $("#run").on("click", function (event) {
   $.ajax({
     type: "POST",
     url: "/code",
-    data: getCode(),
+    data: {
+      "code": getCode()
+    },
     success: function (data, textStatus, jqXHR) {
       console.log(data);
     }
